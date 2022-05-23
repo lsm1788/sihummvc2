375317582e3d2c9808370108dec62ff829c285ba
이승목 <lsm17888@naver.com>
2022-05-23 15:40:35 +0900

    05.23

--- src/main/java/controller/ListController.java
+++ src/main/java/controller/ListController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import domain.BoardInfo;
 import domain.BoardVO;
 import service.ListServiceImpl;
 import service.WriterService;
@@ -37,8 +38,41 @@ public class ListController extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		
 		ListServiceImpl service = new ListServiceImpl();
-		Collection<BoardVO> list = service.read();
-		request.setAttribute("list", list);
+		
+		//페이징
+		int pageCount = 5;
+		int pageRow = 3;//페이지당 보여줄 게시물 갯수
+		int pageNum = 1;
+		int pagingNum = 5;
+		
+		if(request.getParameter("pageNum") != null){
+			pageNum = Integer.parseInt(request.getParameter("pageNum"));
+		}
+		
+		int startPage = (pageNum - 1) * pageRow;
+		
+		BoardInfo boardInfo = service.boardInfo(startPage, pageRow);
+		
+		//글번호 넘버링
+		int totalNum = boardInfo.getTotalRow()-((pageNum - 1) * pageRow);
+		
+		//페이징
+		int startNum = (((pageNum-1)/pagingNum)*pagingNum)+1;
+
+		request.setAttribute("pagingNum", pagingNum);
+		request.setAttribute("startNum", startNum);
+
+		request.setAttribute("pageRow", pageRow);
+		
+		request.setAttribute("totalNum", totalNum);
+		request.setAttribute("boardInfo", boardInfo);
+		request.setAttribute("pageNum", pageNum);
+//		Collection<BoardVO> list = service.read();
+//		int totalRow = service.totalRow();
+//		
+//		request.setAttribute("list", list);
+//		request.setAttribute("totalRow", totalRow);
+		
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
 		dispatcher.forward(request, response);
--- src/main/java/service/ListServiceImpl.java
+++ src/main/java/service/ListServiceImpl.java
@@ -2,6 +2,7 @@ package service;
 
 import java.util.Collection;
 
+import domain.BoardInfo;
 import domain.BoardVO;
 import mapper.ListMapper;
 
@@ -10,6 +11,24 @@ public class ListServiceImpl implements ListService {
 	@Override
 	public Collection<BoardVO> read() {
 		return new ListMapper().read();
+	}
+	
+	public int totalRow() {
+		return new ListMapper().totalRow();
+	}
+
+	public BoardInfo boardInfo() {
+		BoardInfo boardInfo = new BoardInfo();
+		boardInfo.setList(new ListMapper().read());
+		boardInfo.setTotalRow(new ListMapper().totalRow());
+		return boardInfo;
+	}
+
+	public BoardInfo boardInfo(int startPage, int pageRow) {
+		BoardInfo boardInfo = new BoardInfo();
+		boardInfo.setList(new ListMapper().read(startPage, pageRow));
+		boardInfo.setTotalRow(new ListMapper().totalRow());
+		return boardInfo;
 	}
 
 }
